5d7c643fb85e2b4930bf137b271cc90793892e79
JaneIllario <illariojane@gmail.com>
2018-06-15 19:29:01 -0400

    iterator version
    attempting to implement functional programming; not wokring rn

---
+++ doc_string_wk1/iter_doc_string.py
@@ -0,0 +1,17 @@
+import os
+
+files=''
+path = 'C:\Users\janei\OneDrive\Desktop\Projects\github\weekly_challenge\doc_string_wk1\src_txt'
+
+# for file in os.listdir(path):
+# 	doc=open(path+"\\"+file,"r")
+# 	files=files+doc.read()+' '
+
+with open(path+"\\"+iter(os.listdir(path)),"r") as file:
+	files=[files.append(file) for file in file.read()]
+print(files)
+
+save_path = 'C:\Users\janei\OneDrive\Desktop\Projects\github\weekly_challenge\doc_string_wk1'
+
+with open(os.path.join(save_path, "output"), "w+") as output_file:
+	output_file.write(files)
